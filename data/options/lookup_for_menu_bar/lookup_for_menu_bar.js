2a000b064d3c9fbed01e88b2829e28aa947fecf8
Harry <45528888+harry-private@users.noreply.github.com>
2020-12-29 13:17:00 +0530

    changed name of functions and html classed

--- data/options/lookup_for_menu_bar/lookup_for_menu_bar.js
+++ data/options/lookup_for_menu_bar/lookup_for_menu_bar.js
@@ -1,24 +1,20 @@
 (async () => {
     'use strict'
-
-    // CONSIDER Changing panel to navbar
-    // CONSIDER changing createPanel() to insetNavbar()
-    // CONSIDER changing changeQuery() to querySubmit()
     class Lookup {
         async _constructor() {
             this.localStorageData = await lookupUtility.localStorageDataPromise();
             this.body = document.body;
             this.html = document.documentElement;
 
-            this.panel;
-            this.panelSelect = null;
-            this.panelQueryFrom = null;
-            this.panelQueryInput = null;
+            this.navBar;
+            this.select = null;
+            this.from = null;
+            this.input = null;
             this.run();
         }
         run() {
-            this.createPanel();
-            this.changeQuery();
+            this.insertNavbar();
+            this.querySubmitted();
         }
         sourcesOptionsForSelect() {
             let options = '';
@@ -30,31 +26,30 @@
             return options;
         }
 
-        createPanel() {
-            this.panel = document.createElement("div");
-            // CONSIDER Changing panel-select-panel-input-container class name to panel-form-container, or something similar
-            this.panel.insertAdjacentHTML("afterbegin", `
-              <div class="panel-select-panel-input-container">
-                <form class="lookup-form" title="Type your query and press Enter">
-                  <input class="lookup-query-input" placeholder="Type your query and press Enter" autofocus>
+        insertNavbar() {
+            this.navBar = document.createElement("div");
+            this.navBar.insertAdjacentHTML("afterbegin", `
+              <div class="formContainer">
+                <form class="form" title="Type your query and press Enter">
+                  <input class="input" placeholder="Type your query and press Enter" autofocus>
                 </form>
-                <select class="lookup-panel-select lookup-custom-select">${this.sourcesOptionsForSelect()}</select>
+                <select class="select">${this.sourcesOptionsForSelect()}</select>
               </div>
             `);
-            this.panel.classList.add("lookup-panel");
-            this.panelSelect = this.panel.querySelector('.lookup-panel-select');
-            this.panelQueryForm = this.panel.querySelector('.lookup-form');
-            this.panelQueryInput = this.panel.querySelector('.lookup-query-input');
-            this.panelSelect.addEventListener('change', this.changeSource());
-            this.body.appendChild(this.panel);
+            this.navBar.classList.add("navbar");
+            this.select = this.navBar.querySelector('.select');
+            this.form = this.navBar.querySelector('.form');
+            this.input = this.navBar.querySelector('.input');
+            this.select.addEventListener('change', this.sourceChanged());
+            this.body.appendChild(this.navBar);
         }
 
-        changeSource(e) {
+        sourceChanged(e) {
             return () => {
-                if (!this.panel) { return; }
-                let query = this.panelQueryInput.value.trim();
+                if (!this.navBar) { return; }
+                let query = this.input.value.trim();
                 if (!query) { return; }
-                let selectedSource = this.panelSelect.options[this.panelSelect.selectedIndex];
+                let selectedSource = this.select.options[this.select.selectedIndex];
                 let selectedSourceUrl = selectedSource.dataset.url;
                 let url = lookupUtility.createSourceUrlForNewWindow(selectedSourceUrl, query);
                 chrome.runtime.sendMessage({
@@ -66,16 +61,16 @@
             }
 
         }
-        changeQuery() {
-            if (!this.panel) { return; }
-            let queryOld = this.panelQueryInput.value.trim();
+        querySubmitted() {
+            if (!this.navBar) { return; }
+            let queryOld = this.input.value.trim();
 
-            this.panelQueryForm.addEventListener("submit", (e) => {
+            this.form.addEventListener("submit", (e) => {
                 e.preventDefault();
-                let query = this.panelQueryInput.value.trim();
+                let query = this.input.value.trim();
                 if (query == "" || query === queryOld) { return; }
 
-                let selectedSource = this.panelSelect.options[this.panelSelect.selectedIndex];
+                let selectedSource = this.select.options[this.select.selectedIndex];
                 let selectedSourceUrl = selectedSource.dataset.url;
                 if (!selectedSourceUrl) {
                     selectedSourceUrl = this.selectedSource.dataset.url;
